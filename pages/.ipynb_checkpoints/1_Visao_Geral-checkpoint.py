f43b3f840150e3675088c0b480dec0c72b6cbded
pablocssousa <pb.cssousa@gmail.com>
2022-12-16 04:52:54 -0300

    correcao inflecction em visao geral

--- pages/.ipynb_checkpoints/1_Visao_Geral-checkpoint.py
+++ pages/.ipynb_checkpoints/1_Visao_Geral-checkpoint.py
@@ -5,6 +5,7 @@ import csv
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+import inflection
 import folium
 from haversine import haversine
 import streamlit as st
